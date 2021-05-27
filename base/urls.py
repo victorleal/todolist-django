05a29caf09f5c2537b31018bc7b48c5f4791709f
Victor Leal <victorleal18@gmail.com>
2021-05-27 01:08:36 +0000

    Implement new user register form

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterNewUser
 
 urlpatterns = [
     path('login', CustomLoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterNewUser.as_view(), name='register'),
+
     path('', TaskList.as_view(), name='tasks'),
     path('task_create', TaskCreate.as_view(), name='task_create'),
     path('task_update/<int:pk>', TaskUpdate.as_view(), name='task_update'),
--- base/views.py
+++ base/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -15,6 +17,26 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+class RegisterNewUser(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+            
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+
+        if user is not None:
+            login(self.request, user)
+
+        return super(RegisterNewUser, self).form_valid(form)
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
