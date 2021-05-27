587de3883506d9f2c485756e7f870f92764edc7e
Victor Leal <victorleal18@gmail.com>
2021-05-27 00:48:59 +0000

    Login and user access control

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from django.contrib.auth.views import LogoutView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
 urlpatterns = [
+    path('login', CustomLoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task_create', TaskCreate.as_view(), name='task_create'),
     path('task_update/<int:pk>', TaskUpdate.as_view(), name='task_update'),
